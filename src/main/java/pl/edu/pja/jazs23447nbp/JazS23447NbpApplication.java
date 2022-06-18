28c7ef799b30f867e830370ed9cd0587750767e2
Damian Kreft <damiankreft87@gmail.com>
2022-06-18 09:32:52 +0200

    Add project files.

---
+++ src/main/java/pl/edu/pja/jazs23447nbp/JazS23447NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.jazs23447nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS23447NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS23447NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pja/jazs23447nbp/JazS23447NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.jazs23447nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS23447NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
