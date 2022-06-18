16927608969f1f44edb1bdfd14ccd19d44e55238
Damian Kreft <damiankreft87@gmail.com>
2022-06-18 09:45:39 +0200

    Add RestTemplateConfiguration - create bean from RestTemplate.

---
+++ src/main/java/pl/edu/pja/jazs23447nbp/configuration/RestTemplateConfiguration.java
@@ -0,0 +1,13 @@
+package pl.edu.pja.jazs23447nbp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfiguration {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
